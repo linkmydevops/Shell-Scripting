222501554adef456136478252ab9d54a00273701
linkmydevops <francis.jinadu@outlook.com>
2020-06-19 20:36:18 -0400

    .

--- 02.Print.sh
+++ 02.Print.sh
@@ -6,12 +6,12 @@ echo this is how to print
 
 <<syntax
 
-echo -e "line1/nline2"
+echo -e "line1\nline2"
 -e flag is mandatory to print new  lines
 double quotes "" is also mandatory
-/n is also mandatory
+\n is also mandatory - note the backward slash
 
 syntax
 
-echo  -e "Hello/nThis is linkmydevops"
+echo -e "Hello\nThis is linkmydevops"
 
