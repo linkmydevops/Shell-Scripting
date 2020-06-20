74d63a7002bd86afda24ebc448e4015374594234
linkmydevops <francis.jinadu@outlook.com>
2020-06-19 20:43:27 -0400

    .

--- 02.Print.sh
+++ 02.Print.sh
@@ -4,14 +4,17 @@ echo this is how to print
 
 #How to print multiple lines
 
-<<syntax
+<<printmultiplelines
 
 echo -e "line1\nline2"
 -e flag is mandatory to print new  lines
 double quotes "" is also mandatory
 \n is also mandatory - note the backward slash
 
-syntax
+printmultiplelines
+
 
 echo  -e "Hello\nThis is linkmydevops"
+
+
 
