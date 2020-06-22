f25153b807810aa5f1afb6f7100fbbf680837a5c
linkmydevops <francis.jinadu@gmail.com>
2020-06-21 21:56:53 -0400

    Using input in shell scripting

--- Project Roboshop/roboshop.sh
+++ Project Roboshop/roboshop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # COLOR CODES
+
 B="\e[1;34m"
 Y="\e[1;33m"
 R="\e[1;31m"
@@ -9,4 +10,4 @@ N="\e[0m"
 #Note, 1 is for bold feature
 #Remember \e[0m is needed every time to go back to neutral color
 
-echo -e "{$(Y)WARNING$(N)} WARNING"
+echo -e "{$(Y)WARNING$(B)} WARNING"
