9e7bcf5cc6fbab02f2e908804ad4c6017006afeb
linkmydevops <francis.jinadu@outlook.com>
2020-06-22 14:38:18 -0400

    If statement examples

--- Linux Academy Shell Scripting/02. If_Then_Else Statement.sh
+++ Linux Academy Shell Scripting/02. If_Then_Else Statement.sh
@@ -6,8 +6,24 @@
 
 echo "Enter a number from 1 to 3:"
 read VALUE
-if [ "$VALUE -eq 1" ] || [ "$VALUE -eq 2" ] || [ "$VALUE -eq 3" ]; then
+if [ "$VALUE" -eq 1 ] 2>/dev/null || [ "$VALUE" -eq 2 ] 2>/dev/null || [ "$VALUE" -eq 3 ] 2>/dev/null; then
   echo "You entered $VALUE"
-  else
-    echo "You did not follow instructions"
+else
+    echo "You did not follow instructions!"
 fi
+
+## 2>/dev/null is used to redirect and dispose any error output to the /dev/null file.
+##
+
+##USING ELIF
+
+echo "Enter of number from 1 to 3:"
+read VALUE
+if ["$VALUE" -eq 1 ] 2>/dev/null; then
+  echo "You entered #1"
+elif ["$VALUE" -eq 2 ] 2>/dev/null/; then
+  echo "You entered #2"
+elif [ "$VALUE" -eq 3 ] 2>/dull/null; then
+  echo "You entered #3"
+else
+  echo "You did not follow instructions"
