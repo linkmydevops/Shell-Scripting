55e98dc91bdf8252bd76daade1b066e8c2201344
linkmydevops <francis.jinadu@outlook.com>
2020-06-24 11:51:56 -0400

    If statement examples

---
+++ Linux Academy Shell Scripting/04. Case_Statement.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+<<EOR
+
+Case statements are used similar to if else
+They are used when you need to perform/output different things for different situations
+for example, if you enter a, do this, if you enter b do this, if your number if 5 do that etc
+
+EOR
+
+echo "MAIN MENU"
+echo "========="
+echo "1) Choice One"
+echo "2) Choice Two"
+echo "3) Choice Three"
+echo ""
+echo "Enter Choice"
+read MENUCHOICE
+
+case $MENUCHOICE in
+  1)
+   echo "You chose Choice One"
+  2)
+   echo "You chose Choice Two"
+  3)
+   echo "You chose Choice Three"
+  *)
+   echo "You did not follow instructions"
+esac
