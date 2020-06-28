5b63d27fa78e36aedadbe73d3ea4750dd3737cd0
linkmydevops <francis.jinadu@gmail.com>
2020-06-27 20:27:36 -0400

    edit

--- Project Roboshop/roboshop.sh
+++ Project Roboshop/roboshop.sh
@@ -10,12 +10,8 @@ N="\e[0m"
 #Note, 1 is for bold feature
 #Remember \e[0m is needed every time to go back to neutral color
 
-echo -e "[${Y}WARNING${B}] WARNING"
 
-echo -e "\e[34mWARMING"
+echo -e "[${B}INF${N}] MESSAGE"
+echo -e "[${Y}WAR${N}] MESSAGE"
+echo -e "[${R}ERR${N}] MESSAGE"
 
-echo -e "[${B}INFO${N}] MESSAGE"
-echo -e "[${Y}WARNING${N}] MESSAGE"
-echo -e "[${R}ERROR${N}] MESSAGE"
-
-#e
