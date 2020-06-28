9a6eac3f4ffad0221a239529ddc377559e98b261
linkmydevops <francis.jinadu@gmail.com>
2020-06-27 20:55:27 -0400

    edit

---
+++ 05. Functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Variables: If you assign a name to a set of data, it is called variable
+#Function: If you assign a name to a set of command, it is called function
+
+#Syntax
+
+<<comment
+
+function_sample () {
+ commands
+ commands
+ commands
+}
+comment
+
+##To call a function "f"
+
+f() {
+  echo "This is a function"
+}
+
+f
+
